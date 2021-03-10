07562d3d5f700e0d11c836a92c541be7c71deeb0
escmc29 <escmc29@ensc-esil-18.engineering.sfu.ca>
2021-03-09 21:45:47 -0800

    Finished NOR layout

---
+++ inv/hspiceS/schematic/netlist/inverter_inv_schematic.s
@@ -0,0 +1,14 @@
+# File name: inverter_inv_schematic.s.
+# Subcircuit for cell: inv.
+# Generated for: hspiceS.
+# Generated on Mar  3 17:15:11 2021.
+
+M1 Z A GND! GND!  nch  L=180e-9 W=500e-9 AD=480e-9*500e-9 AS=480e-9*500e-9 &
+PD=960e-9+2.0*5000e-9 PS=960e-9+2.0*5000e-9 NRD=270e-9/500e-9 NRS=270e-9/&
+500e-9 M=1.0 
+M0 Z A VDD! VDD!  pch  L=180e-9 W=500e-9 AD=480e-9*500e-9 AS=480e-9*500e-9 &
+PD=960e-9+2.0*5000e-9 PS=960e-9+2.0*5000e-9 NRD=270e-9/500e-9 NRS=270e-9/&
+500e-9 M=1.0 
+
+
+
